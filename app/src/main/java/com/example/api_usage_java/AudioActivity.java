89852a60c123860bde30792cd9c8cd2aebde10ca
Leftddr <qhddnjs75@gmail.com>
2020-12-04 23:42:58 +0900

    ui 수정

--- app/src/main/java/com/example/api_usage_java/AudioActivity.java
+++ app/src/main/java/com/example/api_usage_java/AudioActivity.java
@@ -9,8 +9,10 @@ import android.os.Message;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -24,12 +26,18 @@ import com.naver.speech.clientapi.SpeechRecognitionListener;
 import com.naver.speech.clientapi.SpeechRecognitionResult;
 import com.naver.speech.clientapi.SpeechRecognizer;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import android.os.Handler;
+import android.widget.Toast;
+
 import java.util.logging.LogRecord;
 
 import androidx.annotation.RequiresApi;
@@ -38,7 +46,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class AudioActivity extends Activity {
     private static final String TAG = AudioActivity.class.getSimpleName();
-    private static final String CLIENT_ID = ""; // "내 애플리케이션"에서 Client ID를 확인해서 이곳에 적어주세요.
+    private static final String CLIENT_ID = "n3gce9qog0"; // "내 애플리케이션"에서 Client ID를 확인해서 이곳에 적어주세요.
     private RecognitionHandler handler;
     private NaverRecognizer naverRecognizer;
     private TextView txtResult;
@@ -48,6 +56,7 @@ public class AudioActivity extends Activity {
     private ListView listView;
     private audioAdapter audioadapter;
     private ArrayList<String> audio_text = new ArrayList<>();
+    private long first_click = -1, index = -1;
     // Handle speech recognition Messages.
 
     @RequiresApi(api = Build.VERSION_CODES.Q)
@@ -56,20 +65,21 @@ public class AudioActivity extends Activity {
             case R.id.clientReady: // 음성인식 준비 가능
                 txtResult.setText("Connected");
                 File storageDir = getExternalFilesDir(Environment.DIRECTORY_AUDIOBOOKS);
-                try {
-                    File audio = File.createTempFile("test", "", storageDir);
-                    writer = new AudioWriterPCM(audio.getAbsolutePath());
+                //try {
+                    //File audio = File.createTempFile("test", "", storageDir);
+                    writer = new AudioWriterPCM(storageDir.getAbsolutePath());
+                    System.out.println("-----------------------------------------" + writer + "-------------------------------------");
                     writer.open("Test");
-                } catch (IOException e){
-                    e.printStackTrace();
-                }
+                //} catch (IOException e){
+                //    e.printStackTrace();
+                //}
                 break;
             case R.id.audioRecording:
                 writer.write((short[]) msg.obj);
                 break;
             case R.id.partialResult:
                 mResult = (String) (msg.obj);
-                txtResult.setText("인식중......");
+                txtResult.setText(mResult);
                 break;
             case R.id.finalResult: // 최종 인식 결과
                 SpeechRecognitionResult speechRecognitionResult = (SpeechRecognitionResult) msg.obj;
@@ -108,18 +118,24 @@ public class AudioActivity extends Activity {
         listView = (ListView)findViewById(R.id.listview);
         txtResult = (TextView)findViewById(R.id.txtResult);
         btnStart = (Button) findViewById(R.id.btn_start);
+        //listview를 안보이게
+        listView.setVisibility(View.GONE);
         handler = new RecognitionHandler(this);
         naverRecognizer = new NaverRecognizer(this, handler, CLIENT_ID);
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(!naverRecognizer.getSpeechRecognizer().isRunning()) {
+                    listView.setVisibility(View.GONE);
+                    txtResult.setVisibility(View.VISIBLE);
                     audio_text.clear();
                     mResult = "";
                     txtResult.setText("Connecting...");
                     btnStart.setText(R.string.str_stop);
                     naverRecognizer.recognize();
                 } else {
+                    listView.setVisibility(View.VISIBLE);
+                    txtResult.setVisibility(View.GONE);
                     Log.d(TAG, "stop and wait Final Result");
                     btnStart.setEnabled(false);
                     naverRecognizer.getSpeechRecognizer().stop();
@@ -134,6 +150,39 @@ public class AudioActivity extends Activity {
             audioadapter.addItem(new AudioItem(audio_text.get(i)));
         }
         listView.setAdapter(audioadapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                AudioItem tmp = (AudioItem) audioadapter.getItem(position);
+                if(index == -1 || first_click == -1){
+                    first_click = System.currentTimeMillis();
+                    index = position;
+                }
+                else if(index == position && System.currentTimeMillis() - first_click <= 1000) {
+                    Toast.makeText(getApplicationContext(), "저장합니다.", Toast.LENGTH_LONG).show();
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+                        File storageDir = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+                        try {
+                            long now = System.currentTimeMillis();
+                            Date date = new Date(now);
+                            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+                            String nowTime = sdf.format(date);
+
+                            File text_file = File.createTempFile("test", "_" + nowTime + ".txt", storageDir);
+                            System.out.println("-----------------------" + text_file.getAbsolutePath() + "-----------------------------");
+                            BufferedWriter buf = new BufferedWriter(new FileWriter(text_file.getAbsolutePath(), true));
+                            buf.append(tmp.getText());
+                            buf.close();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                else{
+                    index = position; first_click = System.currentTimeMillis();
+                }
+            }
+        });
     }
     @Override
     protected void onStart() {
